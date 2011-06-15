0745ee5c28b8d2463fe2c7b738ae7eb7d46b6e6f
U-WIN-UMGTUCNDFPS\Administrator <Administrator@WIN-UMGTUCNDFPS.(none)>
2011-06-15 20:50:49 +0200

    init commit

---
+++ war/genfilesforspeedtracer/de/evgeny/classico/server/client/GreetingService_Proxy.java
@@ -0,0 +1,52 @@
+package de.evgeny.classico.server.client;
+
+import com.google.gwt.user.client.rpc.impl.RemoteServiceProxy;
+import com.google.gwt.user.client.rpc.impl.ClientSerializationStreamWriter;
+import com.google.gwt.user.client.rpc.SerializationStreamWriter;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.impl.RequestCallbackAdapter.ResponseReader;
+import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.gwt.user.client.rpc.RpcToken;
+import com.google.gwt.user.client.rpc.RpcTokenException;
+import com.google.gwt.core.client.impl.Impl;
+import com.google.gwt.user.client.rpc.impl.RpcStatsContext;
+
+public class GreetingService_Proxy extends RemoteServiceProxy implements de.evgeny.classico.server.client.GreetingServiceAsync {
+  private static final String REMOTE_SERVICE_INTERFACE_NAME = "de.evgeny.classico.server.client.GreetingService";
+  private static final String SERIALIZATION_POLICY ="C65992C4961AFA338BF64C3A7C69B303";
+  private static final de.evgeny.classico.server.client.GreetingService_TypeSerializer SERIALIZER = new de.evgeny.classico.server.client.GreetingService_TypeSerializer();
+  
+  public GreetingService_Proxy() {
+    super(GWT.getModuleBaseURL(),
+      "greet", 
+      SERIALIZATION_POLICY, 
+      SERIALIZER);
+  }
+  
+  public void greetServer(java.lang.String input, com.google.gwt.user.client.rpc.AsyncCallback callback) {
+    com.google.gwt.user.client.rpc.impl.RemoteServiceProxy.ServiceHelper helper = new com.google.gwt.user.client.rpc.impl.RemoteServiceProxy.ServiceHelper("GreetingService_Proxy", "greetServer");
+    try {
+      SerializationStreamWriter streamWriter = helper.start(REMOTE_SERVICE_INTERFACE_NAME, 1);
+      streamWriter.writeString("java.lang.String/2004016611");
+      streamWriter.writeString(input);
+      helper.finish(callback, ResponseReader.STRING);
+    } catch (SerializationException ex) {
+      callback.onFailure(ex);
+    }
+  }
+  @Override
+  public SerializationStreamWriter createStreamWriter() {
+    ClientSerializationStreamWriter toReturn =
+      (ClientSerializationStreamWriter) super.createStreamWriter();
+    if (getRpcToken() != null) {
+      toReturn.addFlags(ClientSerializationStreamWriter.FLAG_RPC_TOKEN_INCLUDED);
+    }
+    return toReturn;
+  }
+  @Override
+  protected void checkRpcTokenType(RpcToken token) {
+    if (!(token instanceof com.google.gwt.user.client.rpc.XsrfToken)) {
+      throw new RpcTokenException("Invalid RpcToken type: expected 'com.google.gwt.user.client.rpc.XsrfToken' but got '" + token.getClass() + "'");
+    }
+  }
+}
